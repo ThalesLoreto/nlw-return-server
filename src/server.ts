1d9f9beae7c307e589122383125a08ca7f8e4890
ThalesLoreto <tloreto.dev@gmail.com>
2022-05-07 00:18:18 -0300

    add env var port

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
-app.listen(3333, () => console.log('Server Running...'));
+app.listen(process.env.PORT || 3333, () => {
+  console.log('Server Running...');
+});
